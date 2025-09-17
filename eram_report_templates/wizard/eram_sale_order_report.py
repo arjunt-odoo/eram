c137569bbe6d635b1826ac6bacfadd3f865ed7eb
Mohammed <demo.receive.mail@gmail.com>
2025-09-17 11:59:12 +0530

    [UPDT] eram_report_templates: Updated sale order report row merging.

--- eram_report_templates/wizard/eram_sale_order_report.py
+++ eram_report_templates/wizard/eram_sale_order_report.py
@@ -24,7 +24,7 @@ class EramSaleOrderReport(models.TransientModel):
 
     def action_print_report(self):
         if self.type == 'xlsx':
-            domain = [('state', '=', 'sale')]
+            domain = [('state', '!=', 'cancel')]
             if self.from_date and self.to_date:
                 domain.append(('date_order', '>=', self.from_date))
                 domain.append(('date_order', '<=', self.to_date))
@@ -260,6 +260,8 @@ class EramSaleOrderReport(models.TransientModel):
         row = 4
         grand_total_qty = 0
         grand_total_value = 0
+        grand_total_advance = 0
+        grand_total_balance = 0
         si_no = 1
 
         def write_center(sheet, row, col, value, format=None, is_overdue=False):
@@ -352,9 +354,52 @@ class EramSaleOrderReport(models.TransientModel):
 
             order_lines = order.order_line
 
+            # Get shipment status from sale order
+            picking_ids = order.picking_ids.filtered(lambda p: p.state != 'cancel')
+            if picking_ids:
+                if all(picking.state == 'done' for picking in picking_ids):
+                    shipment_status = 'Shipped'
+                elif all(picking.state != 'done' for picking in picking_ids):
+                    shipment_status = 'Not Shipped'
+                else:
+                    shipment_status = 'Partially Shipped'
+            else:
+                shipment_status = 'Nothing to Ship'
+
             invoice_count = len(order_invoices)
             product_count = len(order_lines)
             max_rows_per_order = max(invoice_count, product_count, 1)
+
+            product_distribution = []
+            invoice_distribution = []
+
+            if product_count > 0 and invoice_count > 0:
+                if product_count >= invoice_count:
+                    base_rows_per_invoice = product_count // invoice_count
+                    extra_rows = product_count % invoice_count
+
+                    for i in range(invoice_count):
+                        rows_for_this_invoice = base_rows_per_invoice
+                        if i < extra_rows:
+                            rows_for_this_invoice += 1
+                        invoice_distribution.append(rows_for_this_invoice)
+
+                    product_distribution = [1] * product_count
+
+                else:
+                    base_rows_per_product = invoice_count // product_count
+                    extra_rows = invoice_count % product_count
+
+                    for i in range(product_count):
+                        rows_for_this_product = base_rows_per_product
+                        if i < extra_rows:
+                            rows_for_this_product += 1
+                        product_distribution.append(rows_for_this_product)
+
+                    invoice_distribution = [1] * invoice_count
+            else:
+                product_distribution = [1] * product_count if product_count > 0 else []
+                invoice_distribution = [1] * invoice_count if invoice_count > 0 else []
 
             invoice_data = []
             for invoice in order_invoices:
@@ -430,17 +475,6 @@ class EramSaleOrderReport(models.TransientModel):
                     'payment_terms': payment_terms
                 })
 
-            picking_ids = order.picking_ids.filtered(lambda p: p.state != 'cancel')
-            if picking_ids:
-                if all(picking.state == 'done' for picking in picking_ids):
-                    shipment_status = 'Shipped'
-                elif all(picking.state != 'done' for picking in picking_ids):
-                    shipment_status = 'Not Shipped'
-                else:
-                    shipment_status = 'Partially Shipped'
-            else:
-                shipment_status = 'Nothing to Ship'
-
             if is_even_order:
                 row_fmt = even_row_format
                 currency_fmt = even_currency_format
@@ -462,7 +496,7 @@ class EramSaleOrderReport(models.TransientModel):
                     write_center(sheet, row + i, 2, account_name, row_fmt)
                     write_center(sheet, row + i, 6, order.name, row_fmt)
                     write_center(sheet, row + i, 7, order.date_order, date_fmt)
-                    # Changed from order.note or 'N/A' to just 'N/A'
+                    write_center(sheet, row + i, 21, shipment_status, row_fmt)
                     write_center(sheet, row + i, 22, 'N/A', row_fmt)
                 else:
                     write_center(sheet, row + i, 0, '', row_fmt)
@@ -470,72 +504,123 @@ class EramSaleOrderReport(models.TransientModel):
                     write_center(sheet, row + i, 2, '', row_fmt)
                     write_center(sheet, row + i, 6, '', row_fmt)
                     write_center(sheet, row + i, 7, '', row_fmt)
-                    write_center(sheet, row + i, 22, 'N/A', row_fmt)  # Fill with N/A for all rows
+                    write_center(sheet, row + i, 21, '', row_fmt)
+                    write_center(sheet, row + i, 22, 'N/A', row_fmt)
 
-            for i, line in enumerate(order_lines):
-                if i < max_rows_per_order:
-                    write_center(sheet, row + i, 3, line.product_id.name, row_fmt)
-                    write_center(sheet, row + i, 4, line.e_description or "N/A", row_fmt)
-                    write_center(sheet, row + i, 5, line.product_uom_qty, row_fmt)
-                    grand_total_qty += line.product_uom_qty
+            current_row = 0
+            for product_idx, line in enumerate(order_lines):
+                rows_for_this_product = product_distribution[product_idx] if product_distribution else 1
 
-            for i in range(len(order_lines), max_rows_per_order):
+                for i in range(rows_for_this_product):
+                    if current_row + i < max_rows_per_order:
+                        write_center(sheet, row + current_row + i, 3, line.product_id.name, row_fmt)
+                        write_center(sheet, row + current_row + i, 4, line.e_description or "N/A", row_fmt)
+                        write_center(sheet, row + current_row + i, 5, line.product_uom_qty, row_fmt)
+                        grand_total_qty += line.product_uom_qty
+
+                if rows_for_this_product > 1:
+                    safe_merge(sheet, row + current_row, 3, row + current_row + rows_for_this_product - 1, 3,
+                               line.product_id.name, row_fmt)
+                    safe_merge(sheet, row + current_row, 4, row + current_row + rows_for_this_product - 1, 4,
+                               line.e_description or "N/A", row_fmt)
+                    safe_merge(sheet, row + current_row, 5, row + current_row + rows_for_this_product - 1, 5,
+                               line.product_uom_qty, row_fmt)
+
+                current_row += rows_for_this_product
+
+            for i in range(current_row, max_rows_per_order):
                 write_center(sheet, row + i, 3, '', row_fmt)
                 write_center(sheet, row + i, 4, '', row_fmt)
                 write_center(sheet, row + i, 5, '', row_fmt)
 
-            for i, inv in enumerate(invoice_data):
-                if i < max_rows_per_order:
-                    write_center(sheet, row + i, 8, inv['buyer_order_no'], row_fmt)
-                    write_center(sheet, row + i, 9, inv['invoice_no'], row_fmt)
+            current_row = 0
+            if invoice_count > 0:
+                for invoice_idx, inv in enumerate(invoice_data):
+                    rows_for_this_invoice = invoice_distribution[invoice_idx] if invoice_distribution else 1
 
-                    if inv['invoice_date'] != 'N/A':
-                        write_center(sheet, row + i, 10, inv['invoice_date'], date_fmt)
-                    else:
-                        write_center(sheet, row + i, 10, inv['invoice_date'], row_fmt)
+                    for i in range(rows_for_this_invoice):
+                        if current_row + i < max_rows_per_order:
+                            write_center(sheet, row + current_row + i, 8, inv['buyer_order_no'], row_fmt)
+                            write_center(sheet, row + current_row + i, 9, inv['invoice_no'], row_fmt)
 
-                    write_center(sheet, row + i, 11, inv['invoice_value'], order_currency_format)
-                    write_center(sheet, row + i, 12, inv['payment_terms'], row_fmt)
-                    write_center(sheet, row + i, 13, inv['payment_status'], row_fmt)
-                    write_center(sheet, row + i, 14, inv['advance_payment'], row_fmt)
-                    write_center(sheet, row + i, 15, inv['advance_amount'], order_currency_format)
+                            if inv['invoice_date'] != 'N/A':
+                                write_center(sheet, row + current_row + i, 10, inv['invoice_date'], date_fmt)
+                            else:
+                                write_center(sheet, row + current_row + i, 10, inv['invoice_date'], row_fmt)
 
-                    if isinstance(inv['advance_date'], date) or (
-                            isinstance(inv['advance_date'], str) and inv['advance_date'] != 'N/A'):
-                        write_center(sheet, row + i, 16, inv['advance_date'], date_fmt)
-                    else:
-                        write_center(sheet, row + i, 16, inv['advance_date'], row_fmt)
+                            write_center(sheet, row + current_row + i, 11, inv['invoice_value'], order_currency_format)
+                            write_center(sheet, row + current_row + i, 12, inv['payment_terms'], row_fmt)
+                            write_center(sheet, row + current_row + i, 13, inv['payment_status'], row_fmt)
+                            write_center(sheet, row + current_row + i, 14, inv['advance_payment'], row_fmt)
+                            write_center(sheet, row + current_row + i, 15, inv['advance_amount'], order_currency_format)
 
-                    write_center(sheet, row + i, 17, inv['balance_payment'], order_currency_format)
+                            if isinstance(inv['advance_date'], date) or (
+                                    isinstance(inv['advance_date'], str) and inv['advance_date'] != 'N/A'):
+                                write_center(sheet, row + current_row + i, 16, inv['advance_date'], date_fmt)
+                            else:
+                                write_center(sheet, row + current_row + i, 16, inv['advance_date'], row_fmt)
 
-                    if isinstance(inv['balance_date'], date) or (
-                            isinstance(inv['balance_date'], str) and inv['balance_date'] != 'N/A'):
-                        write_center(sheet, row + i, 18, inv['balance_date'], date_fmt)
-                    else:
-                        write_center(sheet, row + i, 18, inv['balance_date'], row_fmt)
+                            write_center(sheet, row + current_row + i, 17, inv['balance_payment'],
+                                         order_currency_format)
 
-                    if inv['is_overdue']:
-                        write_center(sheet, row + i, 19, inv['payment_due'], red_fmt)
-                    else:
-                        write_center(sheet, row + i, 19, inv['payment_due'], row_fmt)
+                            if isinstance(inv['balance_date'], date) or (
+                                    isinstance(inv['balance_date'], str) and inv['balance_date'] != 'N/A'):
+                                write_center(sheet, row + current_row + i, 18, inv['balance_date'], date_fmt)
+                            else:
+                                write_center(sheet, row + current_row + i, 18, inv['balance_date'], row_fmt)
 
-                    if inv['payment_due_date'] != 'N/A':
-                        write_center(sheet, row + i, 20, inv['payment_due_date'], date_fmt)
-                    else:
-                        write_center(sheet, row + i, 20, inv['payment_due_date'], row_fmt)
+                            if inv['is_overdue']:
+                                write_center(sheet, row + current_row + i, 19, inv['payment_due'], red_fmt)
+                            else:
+                                write_center(sheet, row + current_row + i, 19, inv['payment_due'], row_fmt)
 
-                    write_center(sheet, row + i, 21, shipment_status, row_fmt)
+                            if inv['payment_due_date'] != 'N/A':
+                                write_center(sheet, row + current_row + i, 20, inv['payment_due_date'], date_fmt)
+                            else:
+                                write_center(sheet, row + current_row + i, 20, inv['payment_due_date'], row_fmt)
 
-                    grand_total_value += inv['invoice_value']
+                            grand_total_value += inv['invoice_value'] if i == 0 else 0
+                            grand_total_advance += inv['advance_amount'] if i == 0 else 0
+                            grand_total_balance += inv['balance_payment'] if i == 0 else 0
 
-            for i in range(len(invoice_data), max_rows_per_order):
-                for col in range(8, 22):
+                    if rows_for_this_invoice > 1:
+                        safe_merge(sheet, row + current_row, 8, row + current_row + rows_for_this_invoice - 1, 8,
+                                   inv['buyer_order_no'], row_fmt)
+                        safe_merge(sheet, row + current_row, 9, row + current_row + rows_for_this_invoice - 1, 9,
+                                   inv['invoice_no'], row_fmt)
+                        safe_merge(sheet, row + current_row, 10, row + current_row + rows_for_this_invoice - 1, 10,
+                                   inv['invoice_date'], date_fmt if inv['invoice_date'] != 'N/A' else row_fmt)
+                        safe_merge(sheet, row + current_row, 11, row + current_row + rows_for_this_invoice - 1, 11,
+                                   inv['invoice_value'], order_currency_format)
+                        safe_merge(sheet, row + current_row, 12, row + current_row + rows_for_this_invoice - 1, 12,
+                                   inv['payment_terms'], row_fmt)
+                        safe_merge(sheet, row + current_row, 13, row + current_row + rows_for_this_invoice - 1, 13,
+                                   inv['payment_status'], row_fmt)
+                        safe_merge(sheet, row + current_row, 14, row + current_row + rows_for_this_invoice - 1, 14,
+                                   inv['advance_payment'], row_fmt)
+                        safe_merge(sheet, row + current_row, 15, row + current_row + rows_for_this_invoice - 1, 15,
+                                   inv['advance_amount'], order_currency_format)
+                        safe_merge(sheet, row + current_row, 16, row + current_row + rows_for_this_invoice - 1, 16,
+                                   inv['advance_date'], date_fmt if isinstance(inv['advance_date'], date) or (
+                                    isinstance(inv['advance_date'], str) and inv['advance_date'] != 'N/A') else row_fmt)
+                        safe_merge(sheet, row + current_row, 17, row + current_row + rows_for_this_invoice - 1, 17,
+                                   inv['balance_payment'], order_currency_format)
+                        safe_merge(sheet, row + current_row, 18, row + current_row + rows_for_this_invoice - 1, 18,
+                                   inv['balance_date'], date_fmt if isinstance(inv['balance_date'], date) or (
+                                    isinstance(inv['balance_date'], str) and inv['balance_date'] != 'N/A') else row_fmt)
+                        safe_merge(sheet, row + current_row, 19, row + current_row + rows_for_this_invoice - 1, 19,
+                                   inv['payment_due'], red_fmt if inv['is_overdue'] else row_fmt)
+                        safe_merge(sheet, row + current_row, 20, row + current_row + rows_for_this_invoice - 1, 20,
+                                   inv['payment_due_date'], date_fmt if inv['payment_due_date'] != 'N/A' else row_fmt)
+
+                    current_row += rows_for_this_invoice
+            else:
+                # If no invoices, merge all invoice-related columns into a single blank cell
+                safe_merge(sheet, row, 8, row + max_rows_per_order - 1, 20, '', row_fmt)
+
+            for i in range(current_row, max_rows_per_order):
+                for col in range(8, 21):
                     write_center(sheet, row + i, col, '', row_fmt)
-
-            for i in range(max(invoice_count, product_count), max_rows_per_order):
-                for col in range(3, 22):
-                    if col not in [0, 1, 2, 6, 7, 22]:
-                        write_center(sheet, row + i, col, '', row_fmt)
 
             order_end_row = row + max_rows_per_order - 1
             if order_end_row > order_start_row:
@@ -549,6 +634,7 @@ class EramSaleOrderReport(models.TransientModel):
                 else:
                     safe_merge(sheet, order_start_row, 7, order_end_row, 7, '', row_fmt)
 
+                safe_merge(sheet, order_start_row, 21, order_end_row, 21, shipment_status, row_fmt)
                 safe_merge(sheet, order_start_row, 22, order_end_row, 22, 'N/A', row_fmt)
 
             row += max_rows_per_order
@@ -573,7 +659,13 @@ class EramSaleOrderReport(models.TransientModel):
         for col in range(6, 11):
             write_center(sheet, row, col, '', company_total_qty_format)
         write_center(sheet, row, 11, grand_total_value, company_currency_format)
-        for col in range(12, 23):
+        write_center(sheet, row, 12, '', company_total_qty_format)
+        write_center(sheet, row, 13, '', company_total_qty_format)
+        write_center(sheet, row, 14, '', company_total_qty_format)
+        write_center(sheet, row, 15, grand_total_advance, company_currency_format)
+        write_center(sheet, row, 16, '', company_total_qty_format)
+        write_center(sheet, row, 17, grand_total_balance, company_currency_format)
+        for col in range(18, 23):
             write_center(sheet, row, col, '', company_total_qty_format)
 
         for col, width in enumerate(col_widths):
